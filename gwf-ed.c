340a860aecba5e93ac6b30c6d2bdb0d233ddd542
Heng Li <lh3@me.com>
2022-01-17 15:41:37 -0500

    backup; definitely not working

---
+++ gwf-ed.c
@@ -0,0 +1,168 @@
+#include <assert.h>
+#include <string.h>
+#include "gwfa.h"
+#include "kalloc.h"
+#include "ksort.h"
+
+/**********************
+ * Indexing the graph *
+ **********************/
+
+#define generic_key(x) (x)
+KRADIX_SORT_INIT(gwf64, uint64_t, generic_key, 8)
+
+void gwf_ed_index_arc_core(uint64_t *idx, uint32_t n_vtx, uint32_t n_arc, uint64_t *arc)
+{
+	uint32_t i, st;
+	radix_sort_gwf64(arc, arc + n_arc);
+	for (st = 0, i = 1; i <= n_arc; ++i) {
+		if (i == n_arc || arc[i]>>32 != arc[st]>>32) {
+			uint32_t v = arc[st]>>32;
+			assert(v < n_vtx);
+			idx[v] = (uint64_t)st << 32 | (i - st);
+		}
+	}
+}
+
+void gwf_ed_index(void *km, gwf_graph_t *g)
+{
+	KMALLOC(km, g->aux, g->n_vtx);
+	gwf_ed_index_arc_core(g->aux, g->n_vtx, g->n_arc, g->arc);
+}
+
+void gwf_cleanup(void *km, gwf_graph_t *g)
+{
+	kfree(km, g->aux);
+	g->aux = 0;
+}
+
+/**************************************
+ * Graph WaveFront with edit distance *
+ **************************************/
+
+#include "khashl.h" // make it compatible with kalloc
+#include "kdq.h"
+
+KHASHL_INIT(KH_LOCAL, gwf_set64_t, gwf_set64, uint64_t, kh_hash_dummy, kh_eq_generic)
+
+typedef struct { // a diagonal
+	uint64_t vd; // NB: this wastes 4 bytes due to memory alignment
+	int32_t k;
+} gwf_diag_t;
+
+#define ed_key(x) ((x).vd)
+KRADIX_SORT_INIT(gwf_ed, gwf_diag_t, ed_key, 8)
+
+KDQ_INIT(gwf_diag_t)
+
+static inline void gwf_ed_push(kdq_t(gwf_diag_t) *a, uint32_t v, int32_t d, int32_t k)
+{
+	gwf_diag_t t;
+	t.vd = (uint64_t)v<<32 | (80000000LL + d), t.k = k;
+	*kdq_pushp(gwf_diag_t, a) = t;
+}
+
+// for each diagonal, remove elements that are on on the wave front
+static int32_t gwf_ed_dedup(void *km, int32_t n_a, gwf_diag_t *a)
+{
+	int32_t i, n, st;
+	// TODO: if sorting too slow, add a linear pass to reduce the initial size and/or use stable radix sort
+	radix_sort_gwf_ed(a, a + n_a);
+	for (i = 1, st = 0, n = 0; i <= n_a; ++i) {
+		if (i == n_a || a[i].vd != a[st].vd) {
+			int32_t j, max_j = st;
+			for (j = st + 1; j < i; ++j) // choose the far end (i.e. the wave front)
+				if (a[max_j].k < a[j].k) max_j = j;
+			a[n++] = a[max_j];
+			st = i;
+		}
+	}
+	return n;
+}
+
+gwf_diag_t *gwf_ed_extend(void *km, const gwf_graph_t *g, int32_t ql, const char *q, int32_t v1, int32_t *is_end, int32_t *n_a_, gwf_diag_t *a, gwf_set64_t *h)
+{
+	int32_t i, x, n = *n_a_;
+	kdq_t(gwf_diag_t) *A, *B;
+	gwf_diag_t *b;
+
+	*is_end = 0;
+	for (i = 0, x = 1; i < 32; ++i, x <<= 1)
+		if (x >= n) break;
+	if (i < 4) i = 4;
+	A = kdq_init2(gwf_diag_t, km, i); // $A is a queue. A stack also work, but a queue looks cleaner.
+	for (i = 0; i < n; ++i) *kdq_pushp(gwf_diag_t, A) = a[i];
+	kfree(km, a); // $a is not used as it has been copied to $A
+
+	B = kdq_init2(gwf_diag_t, km, A->bits + 1); // $B is a stack.
+	gwf_set64_clear(h);
+	while (kdq_size(A)) {
+		gwf_diag_t t = *kdq_shift(gwf_diag_t, A);
+		uint32_t v = t.vd >> 32; // vertex
+		int32_t d = (int64_t)((uint32_t)t.vd) - 80000000LL; // diagonal
+		int32_t k = (int32_t)t.k; // wave front position
+		int32_t i = k + d; // query position
+		if (k + 1 < g->len[v] && i + 1 < ql) {
+			while (k + 1 < g->len[v] && i + 1 < ql && g->seq[v][k+1] == q[i+1])
+				++i, ++k;
+			if (k + 1 == g->len[v]) {
+				gwf_ed_push(A, v, d, k);
+			} else {
+				gwf_ed_push(B, v, d-1, k+1);
+				gwf_ed_push(B, v, d,   k+1);
+				gwf_ed_push(B, v, d+1, k);
+			}
+		} else if (i + 1 < ql) { // k + 1 == g->len[v]
+			int32_t ov = g->aux[v]>>32, nv = (int32_t)g->aux[v], j;
+			gwf_ed_push(B, v, d+1, k);
+			for (j = 0; j < nv; ++j) {
+				uint32_t w = (uint32_t)g->arc[ov + j];
+				khint_t itr;
+				itr = gwf_set64_get(h, (uint64_t)w<<32 | (i + 1));
+				if (itr != kh_end(h)) { // visited before from other vertices
+					continue;
+				} else if (q[i + 1] == g->seq[w][0]) {
+					gwf_ed_push(A, v, i+1, 0);
+				} else {
+					gwf_ed_push(B, v, i,   0);
+					gwf_ed_push(B, v, i+1, 0);
+				}
+			}
+		} else if (v == v1) { // i + 1 == ql && k + 1 == g->len[v]
+			*is_end = 1;
+			break;
+		}
+	}
+
+	kdq_destroy(gwf_diag_t, A);
+	n = kdq_size(B), b = B->a, B->a = 0;
+	kdq_destroy(gwf_diag_t, B);
+
+	if (*is_end) {
+		kfree(km, b);
+		*n_a_ = 0;
+		return 0;
+	} else {
+		n = gwf_ed_dedup(km, n, b); // this works only because $B is a stack
+		KREALLOC(km, b, n);
+		*n_a_ = n;
+		return b;
+	}
+}
+
+int32_t gwf_ed(void *km, const gwf_graph_t *g, int32_t ql, const char *q, int32_t v0, int32_t v1)
+{
+	int32_t s = 0, n_a = 1, is_end;
+	gwf_diag_t *a;
+	gwf_set64_t *h;
+	h = gwf_set64_init();
+	KCALLOC(km, a, 1);
+	a[0].vd = (uint64_t)v0<<32 | 80000000LL, a[0].k = 0;
+	while (n_a > 0) {
+		a = gwf_ed_extend(km, g, ql, q, v1, &is_end, &n_a, a, h);
+		if (is_end || n_a == 0) break;
+		++s;
+	}
+	gwf_set64_destroy(h);
+	return is_end? s : -1;
+}
